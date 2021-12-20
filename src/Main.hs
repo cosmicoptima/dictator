dbc3beda9c03ea915a8879f1760facf3308e7aa8
atune <atune313@gmail.com>
2021-12-20 01:03:16 +0000

    make dict modular

--- src/Main.hs
+++ src/Main.hs
@@ -50,6 +50,7 @@ import           Data.Scientific                ( Scientific
                                                 , fromFloatDigits
                                                 )
 import qualified Data.Text                     as T
+import           GenText
 import           Items
 import           Network.Wreq                   ( get
                                                 , post
@@ -62,111 +63,7 @@ import           UnliftIO.Concurrent            ( forkIO
                                                 , threadDelay
                                                 )
 import           UnliftIO.Exception
-
-
--- utilities
-------------
-
-randomChoice :: [a] -> StdGen -> a
-randomChoice xs rng = xs !! n where n = fst $ randomR (0, length xs - 1) rng
-
-data MessageFragment
-    = TextBlock Text
-    | CodeBlock Text
-    deriving (Eq, Show)
-
-fragmentText :: MessageFragment -> Text
-fragmentText (TextBlock t) = t
-fragmentText (CodeBlock t) = t
-
--- | Split a message into segments of code blocks and non-code-blocks.
-messageSplit :: Text -> [MessageFragment]
-messageSplit = filter (not . T.null . fragmentText) . splitMode True
-  where
-    isTick = (== '`')
-
-    splitMode mode msg = if not $ T.null msg
-        then case T.head msg of
-            '`' -> splitMode (not mode) . tail' $ msg
-            _ ->
-                ctor (T.takeWhile (not . isTick) msg)
-                    : (splitMode mode . T.dropWhile (not . isTick) $ msg)
-        else []
-        where ctor = if mode then TextBlock else CodeBlock
-    tail' msg = if T.null msg then T.empty else T.tail msg
-
--- | Filter a message into dictator's voice, excluding code blocks.
-voiceFilter :: Text -> Text
-voiceFilter = T.concat . map format . messageSplit
-  where
-    format (TextBlock t) = "__**" <> (T.strip . T.toUpper) t <> "**__"
-    format (CodeBlock t) = "```" <> t <> "```"
-
--- | Tokenize a message into individual words.
-tokenizeMessage :: Text -> [Text]
-tokenizeMessage =
-    words
-        . T.filter (not . isPunc)
-        . T.concat
-        . map fragmentText
-        . filter (not . isCode)
-        . messageSplit
-  where
-    punc :: String
-    punc = "!?{}&>\"()|<[@]_+*:^p=;\\#£-/~%,.'"
-    isPunc p = p `elem` punc
-    -- Probably something built in to do this kind of work
-    isCode (CodeBlock _) = True
-    isCode _             = False
-
--- | Randomly choose true/false conveniently given a probability in [0.0, 1.0]
-odds :: Double -> StdGen -> Bool
-odds chance = (chance >) . fst . random
-
--- GPT
-------
-
-data GPTOpts = GPTOpts
-    { temperature :: Scientific
-    , topK        :: Int
-    , topP        :: Scientific
-    }
-
-instance Default GPTOpts where
-    def = GPTOpts { temperature = 0.8, topK = 40, topP = 1 }
-
-newtype TextSynthRes = TextSynthRes { fromGPTRes :: Text }
-instance FromJSON TextSynthRes where
-    parseJSON =
-        withObject "TextSynthRes" ((.: "text") >=> return . TextSynthRes)
-
-getGPT :: Text -> DH Text
-getGPT = getGPTWith def
-
-getGPTWith :: GPTOpts -> Text -> DH Text
-getGPTWith GPTOpts { temperature = t, topK = k, topP = p } prompt = do
-    res <- liftIO $ post
-        "https://bellard.org/textsynth/api/v1/engines/gptj_6B/completions"
-        (object
-            [ ("prompt"     , String prompt)
-            , ("seed"       , Number 0)
-            , ("stream"     , Bool False)
-            , ("temperature", Number t)
-            , ("top_k"      , Number (intToSci k))
-            , ("top_p"      , Number p)
-            ]
-        )
-    either (debugDie . fromString) (return . fromGPTRes)
-        . eitherDecode
-        . view responseBody
-        $ res
-    where intToSci = (fromFloatDigits :: Double -> Scientific) . toEnum
-
-makePrompt :: [Text] -> Text
-makePrompt = (<> "\n-") . unlines . map (" -" <>)
-
-getGPTFromExamples :: [Text] -> DH Text
-getGPTFromExamples = getGPT . makePrompt
+import           Utils
 
 -- Discord
 ----------

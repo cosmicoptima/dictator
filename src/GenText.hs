069e9cd852cfaa234b88d6892ac2157afcbfe4fb
Celeste Drummond <parafactual@gmail.com>
2021-12-24 23:11:15 -0600

    more debug

--- src/GenText.hs
+++ src/GenText.hs
@@ -23,13 +23,13 @@ import           Data.Scientific                ( Scientific
                                                 , fromFloatDigits
                                                 )
 import           DiscordUtils
+import           Network.HTTP.Client            ( HttpException )
 import           Network.Wreq                   ( defaults
                                                 , header
                                                 , post
                                                 , postWith
                                                 , responseBody
                                                 )
-import           Prelude                        ( IOError )
 import           UnliftIO
 
 
@@ -93,23 +93,22 @@ instance FromJSON AI21Res where
 
 getJ1 :: Text -> DH Text
 getJ1 prompt = do
-    debugPutStr "before reading file"
-    apiKey <-
-        (readFile "j1key.txt" <&> fromString)
-            `catch` (\(e :: IOError) -> debugPrint e >> return "")
-    debugPutStr "after reading file, before making request"
-    res <- liftIO $ postWith
-        (defaults & header "Authorization" .~ ["Bearer " <> apiKey])
-        "https://api.ai21.com/studio/v1/j1-jumbo/complete"
-        (object
-            [ ("prompt"     , String prompt)
-            , ("numResults" , Number 1)
-            , ("maxTokens"  , Number 8)
-            , ("topKReturn" , Number 40)
-            , ("temperature", Number 1)
-            ]
-        )
-    debugPutStr "after making request, before parsing request"
+    apiKey <- readFile "j1key.txt" <&> fromString
+    res    <-
+        liftIO
+                (postWith
+                    (defaults & header "Authorization" .~ ["Bearer " <> apiKey])
+                    "https://api.ai21.com/studio/v1/j1-jumbo/complete"
+                    (object
+                        [ ("prompt"     , String prompt)
+                        , ("numResults" , Number 1)
+                        , ("maxTokens"  , Number 8)
+                        , ("topKReturn" , Number 40)
+                        , ("temperature", Number 1)
+                        ]
+                    )
+                )
+            `catch` (\(e :: HttpException) -> debugDie . show $ e)
     either (debugDie . fromString) (return . fromJ1Res)
         . eitherDecode
         . view responseBody

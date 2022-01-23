34728bacbf497b71755e379a3fdf282f4e0d7eaa
atune <atune313@gmail.com>
2022-01-23 22:28:21 +0000

    extremely better twitter auth stuff please dont run any of this stuff its just for testing okay

--- src/Utils/Misc.hs
+++ src/Utils/Misc.hs
@@ -29,7 +29,9 @@ import           Discord.Types
 import           Control.Lens
 import           Control.Monad.Except           ( MonadError(throwError) )
 import           Control.Monad.Random           ( newStdGen )
+import qualified Data.ByteString.Char8         as BS
 import           Data.Default                   ( Default(def) )
+import           Data.List                      ( (!!) )
 import qualified Data.Text                     as T
 import qualified Database.Redis                as DB
 import           Network.HTTP.Client            ( defaultManagerSettings )
@@ -40,16 +42,32 @@ import           UnliftIO
 import           UnliftIO.Concurrent            ( threadDelay )
 import           Web.Twitter.Conduit
 
-twitterTokens :: IO OAuth
-twitterTokens =
-    return $ twitterOAuth { oauthConsumerKey = "", oauthConsumerSecret = "" }
+twitterThings :: IO [ByteString]
+twitterThings = BS.lines <$> readFileBS "twitter.txt"
 
+-- API keys and secrets.
+twitterTokens :: IO OAuth
+twitterTokens = do
+    file <- twitterThings
+    return $ twitterOAuth { oauthConsumerKey    = file !! 1
+                          , oauthConsumerSecret = file !! 2
+                          }
+
+-- OAuth user context
+-- "Allows an authorized Twitter App to perform a Twitter action on behalf of an account"
+-- Basically, user tokens and secrets.
 twitterCredentials :: IO Credential
-twitterCredentials =
-    return $ Credential [("oauth_token", ""), ("oauth_token_secret", "")]
+twitterCredentials = do
+    file <- twitterThings
+    return $ Credential
+        [("oauth_token", file !! 3), ("oauth_token_secret", file !! 4)]
 
 makeTwitter :: IO TWInfo
-makeTwitter = do undefined
+makeTwitter = do
+    tokens      <- twitterTokens
+    credentials <- twitterCredentials
+    let token = def { twOAuth = tokens, twCredential = credentials }
+    return $ TWInfo token Nothing
 
 sendTweet :: Text -> DictM ()
 sendTweet tweet = do
@@ -57,9 +75,3 @@ sendTweet tweet = do
     manager <- asks envTwManager
     call'_ info manager $ statusesUpdate tweet
     where call'_ info mgr = void . liftIO . call info mgr
-
-    -- tokens      <- twitterTokens
-    -- 
-    -- credentials <- twitterCredentials
-    -- let token = def { twOAuth = tokens, twCredential = credentials }
-    --     info  = TWInfo token Nothing

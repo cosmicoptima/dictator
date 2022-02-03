d97d951b52ae0ad1dcff5ee292aa004fac518676
celeste drummond <parafactual@gmail.com>
2022-02-03 15:18:20 -0600

    sqoqijuhdhfbh

--- src/Utils/Twitter.hs
+++ src/Utils/Twitter.hs
@@ -1,4 +1,3 @@
-
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE MultiWayIf          #-}
@@ -36,28 +35,27 @@ import           Utils.Discord                  ( sendMessageToLogs )
 twitterAuth :: IO TwitterAuth
 twitterAuth = do
     -- Remove comment lines starting with #
-    twitterLines <- filter (not . BS.isPrefixOf "#") . BS.lines <$> readFileBS
-        "twitter.txt"
-    let [apiKey, apiSecret, userToken, tokenSecret] = take 4 twitterLines
-    return $ TwitterAuth { twAuthApiKey      = apiKey
-                         , twAuthApiSecret   = apiSecret
-                         , twAuthUserToken   = userToken
-                         , twAuthTokenSecret = tokenSecret
-                         }
+  twitterLines <- filter (not . BS.isPrefixOf "#") . BS.lines <$> readFileBS
+    "twitter.txt"
+  let [apiKey, apiSecret, userToken, tokenSecret] = take 4 twitterLines
+  return $ TwitterAuth { twAuthApiKey      = apiKey
+                       , twAuthApiSecret   = apiSecret
+                       , twAuthUserToken   = userToken
+                       , twAuthTokenSecret = tokenSecret
+                       }
 
-
-sendTweet :: Text -> DictM ()
+sendTweet :: Text -> DictM Int
 sendTweet tweet = do
-    TwitterAuth apiKey apiSecret userToken tokenSecret <- asks envTw
-    let opts =
-            defaults & auth ?~ oauth1Auth apiKey apiSecret userToken tokenSecret
-        tweet' = String $ T.take 280 tweet
-    res <- liftIO $ postWith opts
-                             "https://api.twitter.com/2/tweets"
-                             (object [("text", tweet')])
-    sendMessageToLogs $ show res
-    let parsed :: Maybe Value = decode (res ^. responseBody)
-        tweetId = parsed ^? _Just . key "data" . key "id" . _String
-    maybe (throwError $ Complaint "The bird is dead.")
-          return
-          (tweetId >>= readMay . toString)
+  TwitterAuth apiKey apiSecret userToken tokenSecret <- asks envTw
+  let opts =
+        defaults & auth ?~ oauth1Auth apiKey apiSecret userToken tokenSecret
+      tweet' = String $ T.take 280 tweet
+  res <- liftIO $ postWith opts
+                           "https://api.twitter.com/2/tweets"
+                           (object [("text", tweet')])
+  sendMessageToLogs $ show res
+  let parsed :: Maybe Value = decode (res ^. responseBody)
+      tweetId               = parsed ^? _Just . key "data" . key "id" . _String
+  maybe (throwError $ Complaint "The bird is dead.")
+        return
+        (tweetId >>= readMay . toString)

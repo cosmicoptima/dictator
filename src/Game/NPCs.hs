9d8a9cc269381af8e1faab3925b4e9ce0c0523f1
celeste drummond <parafactual@gmail.com>
2022-02-12 23:08:06 -0600

    ,

--- src/Game/NPCs.hs
+++ src/Game/NPCs.hs
@@ -4,8 +4,8 @@
 {-# LANGUAGE QuasiQuotes       #-}
 
 module Game.NPCs
-    ( npcSpeak
-    ) where
+  ( npcSpeak
+  ) where
 
 import           Relude                  hiding ( many )
 
@@ -30,63 +30,62 @@ import           Utils                          ( randomImage )
 
 
 data MemoriesInput = MemoriesInput
-    { miMessages :: [Text]
-    , miMemories :: [Text]
-    }
-    deriving Generic
+  { miMessages :: [Text]
+  , miMemories :: [Text]
+  }
+  deriving Generic
 
 instance ToJSON MemoriesInput
 
 newtype MemoriesOutput = MemoriesOutput (Maybe Text)
 
 instance FromJSON MemoriesOutput where
-    parseJSON =
-        withObject "MemoriesOutput" $ \o -> MemoriesOutput <$> o .:? "memory"
+  parseJSON =
+    withObject "MemoriesOutput" $ \o -> MemoriesOutput <$> o .:? "memory"
 
 
 npcSpeak :: ChannelId -> Text -> DictM ()
 npcSpeak channel npc = do
-    messages <- reverse . filter (not . T.null . messageText) <$> restCall'
-        (GetChannelMessages channel (50, LatestMessages))
+  messages <- reverse . filter (not . T.null . messageText) <$> restCall'
+    (GetChannelMessages channel (50, LatestMessages))
 
-    npcData <- getNPC npc
-    avatar  <- case npcData ^. npcAvatar of
-        Just av -> return av
-        Nothing -> do
-            avatar <- encodeAvatarData <$> randomImage
-            setNPC npc $ npcData & npcAvatar ?~ avatar
-            return avatar
+  npcData <- getNPC npc
+  avatar  <- case npcData ^. npcAvatar of
+    Just av -> return av
+    Nothing -> do
+      avatar <- encodeAvatarData <$> randomImage
+      setNPC npc $ npcData & npcAvatar ?~ avatar
+      return avatar
 
-    let memories = npcData ^. npcMemories . to Set.elems
-    liftIO $ encodeFile "python/input.json"
-                        (MemoriesInput (map messageText messages) memories)
+  let memories = npcData ^. npcMemories . to Set.elems
+  liftIO $ encodeFile "python/input.json"
+                      (MemoriesInput (map messageText messages) memories)
 
-    (exitCode, _, stderr_) <- readProcess
-        $ proc "python3" ["python/memories.py"]
-    when (exitCode /= ExitSuccess) (throwError . Fuckup . decodeUtf8 $ stderr_)
+  (exitCode, _, stderr_) <- readProcess $ proc "python3" ["python/memories.py"]
+  when (exitCode /= ExitSuccess) (throwError . Fuckup . decodeUtf8 $ stderr_)
 
-    memoryJSON <- lift $ readFileBS "python/output.json"
-    let memoryEither = eitherDecodeStrict memoryJSON
-    memory <- case memoryEither of
-        Left  err                     -> throwError . Fuckup . fromString $ err
-        Right (MemoriesOutput memory) -> pure memory
+  memoryJSON <- lift $ readFileBS "python/output.json"
+  let memoryEither = eitherDecodeStrict memoryJSON
+  memory <- case memoryEither of
+    Left  err                     -> throwError . Fuckup . fromString $ err
+    Right (MemoriesOutput memory) -> pure memory
 
-    let thought = maybe
-            ""
-            (\m ->
-                [i|(#{npc} thinks: "#{m}")\n(#{npc} decides to talk about this)\n|]
-            )
-            memory
-        prompt =
-            T.concat (map renderMessage messages) <> thought <> npc <> " says:"
+  let
+    thought = maybe
+      ""
+      (\m -> [i|(#{npc} thinks: "#{m}")\n(#{npc} decides to talk about this)\n|]
+      )
+      memory
+    prompt =
+      T.concat (map renderMessage messages) <> thought <> npc <> " says:"
 
-    output <- getJ1With (J1Opts 1.05 0.9) 32 prompt <&> parse parser ""
-    case output of
-        Left  f -> throwError $ Fuckup (show f)
-        Right t -> sendWebhookMessage channel t (npc <> " (0)") (Just avatar)
+  output <- getJ1With (J1Opts 0.95 0.9) 32 prompt <&> parse parser ""
+  case output of
+    Left  f -> throwError $ Fuckup (show f)
+    Right t -> sendWebhookMessage channel t (npc <> " (0)") (Just avatar)
 
-  where
-    renderMessage m =
-        (userName . messageAuthor) m <> " says: " <> messageText m <> "\n"
+ where
+  renderMessage m =
+    (userName . messageAuthor) m <> " says: " <> messageText m <> "\n"
 
-    parser = fromString <$> many (noneOf "\n")
+  parser = fromString <$> many (noneOf "\n")
